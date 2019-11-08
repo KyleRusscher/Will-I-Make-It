a47b9b2918d4ed59094bdbfc374e48f2f1f7c940
Kyle Russcher <russchky@mail.gvsu.edu>
2019-11-08 13:52:23 -0500

    added transition to select vehicle screen

--- app/src/main/java/com/example/semesterproject/MainActivity.java
+++ app/src/main/java/com/example/semesterproject/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.semesterproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+//TODO change java version to 1.8
+public static final int selection = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button x = (Button)findViewById(R.id.selectVehicle);
+
+        x.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View e) {
+                System.out.println("Button was clicked here");
+                Intent intent = new Intent(MainActivity.this, SelectVehicle.class);
+                startActivityForResult(intent, selection);
+            }
+        });
     }
+
+
 }
